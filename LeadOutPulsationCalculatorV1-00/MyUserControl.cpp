63e8424ea6bce2f78fa2b602f153442d60831407
paul knell <paul knell>
2017-07-09 21:17:39 +0000

    Initial Commit of LeaOutPulsationCalculator

---
+++ LeadOutPulsationCalculatorV1-00/MyUserControl.cpp
@@ -0,0 +1,13 @@
+#include "MyUserControl.h"
+
+using namespace System; 
+using namespace System::Windows::Forms; 
+int main() 
+{ 
+	Application::EnableVisualStyles(); 
+	Application::SetCompatibleTextRenderingDefault(false); 
+	LeadOutPulsationCalculatorV100::MyUserControl MyUserControl;
+	LeadOutPulsationCalculatorV100::MyUserControl Run();
+	return(0);
+}
+ 
